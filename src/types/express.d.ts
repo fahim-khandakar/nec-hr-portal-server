63d93adb5ef770e33bc8221eb34f496f7ede5c4a
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-01-26 14:38:11 +0600

    feat: add authentication middleware and user retrieval endpoint; update dependencies

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,18 +1,8 @@
+import express from "express";
 import { User } from "@prisma/client";
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: User;
-    }
+declare module "express" {
+  export interface Request {
+    user: User;
   }
 }
-
-// import express from "express";
-// import { User } from "@prisma/client";
-
-// declare module "express" {
-//   export interface Request {
-//     user: User;
-//   }
-// }
